d88dca111b9471eb877b442234f0712b3a0dd714
Phanyazuki <160076934+Phanyazuki@users.noreply.github.com>
2024-02-26 01:39:38 +0300

    Add files via upload
    Python Week 1 Assignment

---
+++ User.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+
+# Asking For User Details
+Name = "Rogers Lukwiya"
+Age = "38"
+Location = "Nairobi"
+
+#Printing Personalized Message
+print(f"Hello {Name}, you are {38} years old and live in {Location}.")
